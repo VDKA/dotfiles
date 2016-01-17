214a8fd603c284847ed79b777ac9904a0a89f3af
Ethan Jackwitz <ethanjackwitz@gmail.com>
2016-01-18 08:42:03 +1000

    Fixed OMF setting absolute path to install, breaking my dotfiles across machines.

--- linked/.config/fish/config.fish
+++ linked/.config/fish/config.fish
@@ -1,5 +1,5 @@
 # Path to Oh My Fish install.
-set -gx OMF_PATH "/Users/Ethan/.local/share/omf"
+set -gx OMF_PATH "$USER/.local/share/omf"
 
 # Customize Oh My Fish configuration path.
 #set -gx OMF_CONFIG "/Users/Ethan/.config/omf"
