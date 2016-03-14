1dd6e372ed99371c0d710f665118cf59155e7818
Ethan Jackwitz <ethanjackwitz@gmail.com>
2016-03-14 11:45:30 +1000

    Removed rbenv path

--- linked/.config/fish/config.fish
+++ linked/.config/fish/config.fish
@@ -2,7 +2,7 @@ set fisher_home ~/.local/share/fisherman
 set fisher_config ~/.config/fisherman
 source $fisher_home/config.fish
 
-set -U fish_user_paths /Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin $HOME/.rbenv/bin $HOME/.dotfiles/bin
+set -U fish_user_paths /Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin $HOME/.dotfiles/bin
 
 eval $HOME/.base16_theme
 
